15bd73ab573b79dca398508c459b97a3fa9e2a08
Achanandhi.M <110651321+Achanandhi24@users.noreply.github.com>
2023-02-26 21:15:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let myAge,myBirthYear,currentYear;
+myBirthYear=2003;//Assign your birth year
+currentYear=2023;//Assign the current year
+myAge=currentYear-myBirthYear;
+console.log("Your age is:" ,myAge);
+
